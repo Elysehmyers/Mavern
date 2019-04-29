84abe1f25f39d0ee47ae0d7fb4773e4b39bf4c83
Elyse Myers <elysehmyers@gmail.com>
2019-04-28 19:24:47 -0500

    updated site

--- wp-content/themes/astra/header.php
+++ wp-content/themes/astra/header.php
@@ -21,6 +21,15 @@
 
 <?php wp_head(); ?>
 <?php astra_head_bottom(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-137964660-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-137964660-1');
+</script>
 </head>
 
 <body <?php astra_schema_body(); ?> <?php body_class(); ?>>
---
+++ wp-content/uploads/custom-css-js/974.js
@@ -0,0 +1,36 @@
+<!-- start Simple Custom CSS and JS -->
+
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+If you are using the jQuery library, then don't forget to wrap your code inside jQuery.ready() as follows:
+
+jQuery(document).ready(function( $ ){
+    // Your code in here
+});
+
+--
+
+If you want to link a JavaScript file that resides on another server (similar to
+<script src="https://example.com/your-js-file.js"></script>), then please use
+the "Add HTML Code" page, as this is a HTML code that links a JavaScript file.
+
+End of comment */ 
+
+<!-- end Simple Custom CSS and JS -->
